52a020c32ad815c799151d122b37d4f3050b86a7
Anmol Jande <jandanmol@gmail.com>
2019-01-05 04:03:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@
 def turn_count(board)
   count=0
 board.each do |val|
-  if(val=="X" || val=="O")
-    count+=1 
-  end 
-end   
+   if(val=="X" || val=="O")
+     count+=1 
+   end 
+ end   
 return count   
 end
